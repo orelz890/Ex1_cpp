5cec6ad52419b31bfcf1c160354035afe6c6d60f
orel <orelzx13@gmail.com>
2022-03-09 00:02:16 +0200

    first

---
+++ mat.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "mat.hpp"
+using namespace std;
+namespace ariel{
+    string mat(int length, int height, char sign1, char sign2) {
+        string ans = "ans";
+        return ans;
+    }
+}
+
+
+
---
+++ mat.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+namespace ariel
+{
+    string mat(int length, int height, char sign1, char sign2);
+}
